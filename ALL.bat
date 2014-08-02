88c5b8e6d25403f2dadcbab5e5395dff715d2f81
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2014-08-02 09:54:14 +0700

    First Commit From Old Service-pwdsthai Project

---
+++ ALL.bat
@@ -0,0 +1,5 @@
+start java -jar selenium-server-standalone-2.35.0.jar
+cd "C:\xampp\htdocs\service-pwdsthai-yii\protected\tests"
+call phpunit unit
+call phpunit functional
+pause
---
+++ Device.bat
@@ -0,0 +1,3 @@
+cd "C:\xampp\htdocs\service-pwdsthai-yii\protected\tests"
+call phpunit unit/DeviceTest.php
+pause
